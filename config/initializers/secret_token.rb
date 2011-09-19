49319a74a87c3ecaf986383f3de5ff47a80c0aef
Rafal Babinicz <rafal.babinicz@gmail.com>
2011-09-19 16:37:05 +0200

    Rails 3.1.0 app created with -O (without ActiveRecord) option

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydra::Application.config.secret_token = '60e1d6d5116b69f0024f8ce91ebff2b826b74006ef95a4a240242e130f28beb879465bbdc4af7e414309e12363096f5b98c2e5ea7c30f93536c21447a7e970db'
